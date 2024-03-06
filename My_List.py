546b8b2a63ac516e02585fb9200b3e1b8ad03cd5
Benard Oseko Luka <bluka@uonbi.ac.ke>
2024-03-06 13:56:38 +0300

    My_list assign.

---
+++ My_List.py
@@ -0,0 +1,9 @@
+My_list= []
+My_list.extend([10, 20, 30, 40])
+My_list.insert(1,15)
+My_list.extend([50, 60, 70])
+My_list.pop()
+My_list.sort()
+index_of_30 = My_list.index(30)
+print("index of 30 in my_list:", index_of_30)
+print("Final My list:", My_list)
